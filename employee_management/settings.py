886a64543ca98f83f93ae24d5084fbde1448c28f
beyong2023 <beyong2023@gmail.com>
2023-02-06 18:48:58 +0800

    final_demo: 2.0

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'employee_management',
         'USER': 'root',
-        'PASSWORD': 'Wgf720130601',
+        'PASSWORD': 'XXXXXXXX',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
